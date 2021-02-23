4ed73281f6023574abd2187b0cfc2021fa513445
Israel Ordonez <israelordonez202@outlook.com>
2021-02-23 21:30:00 +0800

    removed items

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { EmployeeComponent } from './employee/employee.component'
+import { EmployeeComponent } from './employee/employee.component';
+import { CreateEmployeeComponent } from './components/EmployeeComponents/create-employee/create-employee.component';
+import { EmployeeListComponent } from './components/EmployeeComponents/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './components/EmployeeComponents/update-employee/update-employee.component';
+import { DeleteEmployeeComponent } from './components/EmployeeComponents/delete-employee/delete-employee.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent,
+    DeleteEmployeeComponent
   ],
   imports: [
     BrowserModule,
