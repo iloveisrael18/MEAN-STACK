bdff0ecfe4da8e69c827a0fc3901be00840de4bc
Israel Ordonez <israelordonez202@outlook.com>
2021-02-23 21:34:04 +0800

    added components

---
+++ AngularApp/src/app/components/EmployeeComponents/employee.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { IEmployee } from './employee'
+import { catchError, retry } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private _url = 'http://localhost:3000/employee'
+
+  constructor(private http: HttpClient) { }
+
+  getEmployees(): Observable<IEmployee[]>{
+    return this.http.get<IEmployee[]>(this._url).pipe(catchError(this.errorHandler));
+
+  }
+
+  errorHandler(error: HttpErrorResponse){
+      return throwError(error.message);
+  }
+}
---
+++ AngularApp/src/app/components/EmployeeComponents/employee.ts
@@ -0,0 +1,8 @@
+export interface IEmployee{
+    _id: string,
+    name: string,
+    position: string,
+    office: string,
+    salary: string
+
+}
