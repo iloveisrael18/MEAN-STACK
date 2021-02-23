082bd8801fe24fd360d4df7a633437262b6d1480
Israel Ordonez <israelordonez202@outlook.com>
2021-02-23 21:31:10 +0800

    removed employee

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { EmployeeComponent } from './employee/employee.component';
 import { CreateEmployeeComponent } from './components/EmployeeComponents/create-employee/create-employee.component';
 import { EmployeeListComponent } from './components/EmployeeComponents/employee-list/employee-list.component';
 import { UpdateEmployeeComponent } from './components/EmployeeComponents/update-employee/update-employee.component';
@@ -12,7 +11,6 @@ import { DeleteEmployeeComponent } from './components/EmployeeComponents/delete-
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent,
     CreateEmployeeComponent,
     EmployeeListComponent,
     UpdateEmployeeComponent,
